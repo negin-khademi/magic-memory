fdb735502f3a8efff44f07a2ee1fad08e5a59b75
Negin <negin.khademi@gmail.com>
2024-11-18 12:29:49 +0330

    shuffling cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
 
-import React from 'react';
+import React, { useState } from 'react';
+
+const cardImages = [
+  { src: '/img/helmet.png' },
+  { src: '/img/position.png' },
+  { src: '/img/ring-1.png' },
+  { src: '/img/scroll-1.png' },
+  { src: '/img/shield-1.png' },
+  { src: '/img/sword-1.png' },
+];
+
 export default function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
