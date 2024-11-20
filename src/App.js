936271186112eb7916f3799f895354558dcfbcdc
Negin <negin.khademi@gmail.com>
2024-11-20 14:16:00 +0330

    modify img

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import React, { useEffect, useState } from 'react';
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { src: '/img/helmet-1.png', matched: false },
-  { src: '/img/potion-1.png', matched: false },
-  { src: '/img/ring-1.png', matched: false },
-  { src: '/img/scroll-1.png', matched: false },
-  { src: '/img/shield-1.png', matched: false },
-  { src: '/img/sword-1.png', matched: false },
+  { src: 'img/helmet-1.png', matched: false },
+  { src: 'img/potion-1.png', matched: false },
+  { src: 'img/ring-1.png', matched: false },
+  { src: 'img/scroll-1.png', matched: false },
+  { src: 'img/shield-1.png', matched: false },
+  { src: 'img/sword-1.png', matched: false },
 ];
 
 export default function App() {
